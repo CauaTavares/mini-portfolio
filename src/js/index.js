94e65432356392d4aaed313e52307004bead7709
CauaTavares <cauaxd2010@outlook.com>
2022-10-15 21:01:27 -0300

    151021

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const abas = document.querySelectorAll(".aba");
+console.log(abas);
+
+abas.forEach(aba => {
+    aba.addEventListener("click", function(){
+        const abaSelecionada = document.querySelector(".aba.selecionado")
+        abaSelecionada.classList
+    })
+})
